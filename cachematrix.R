b0de2e38e39b35116a6c233d7065ff40d4898245
Mait22 <maitmetelitsa@gmail.com>
2014-07-27 17:18:24 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -18,7 +18,13 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Function checks if the value of inverse in global space is equal to "NULL". 
+## If it is not and the inverse for given matrix has been already found it will not re-calculate the 
+## inverse of matrix rather the cached result stored is retrieved and returned to user.
+## If the inverse equals "NULL" this means the inverse for given matrix has not been calculated. 
+## In this case the inverse of matrix will be calculated by using solve function and global variable inverse 
+## value will be set
+## from NULL to the calculated inverse value of matrix using function setinverse. 
 
 cacheSolve <- function(x, ...) {
         inverse <- x$getinverse()
